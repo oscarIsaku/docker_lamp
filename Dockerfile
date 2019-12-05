0da935175c216262a5db80f02c0431c581fb85cb
isaac <tasacata_sorte@yahoo.co.jp>
2019-12-06 01:17:09 +0900

    DB接続ファイル&フォーム入力画面作成

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM php:7.0-apache
 
-RUN apt-get update
+RUN apt-get update && docker-php-ext-install pdo_mysql
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<h2>Practice</h2>
+<pre>
+<?php
+//DB情報を読み込み
+require_once("database.php");
+
+$records = $dbh->query('SELECT * FROM my_items');
+
+while ($record = $records->fetch()) {
+  print($record['item_name']. "\n");
+}
+?>
+</pre>
+</html>
